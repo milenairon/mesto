27bd5347b11783412b45e78f3dca9cd20be483df
Elena Vasileva <milena-iron@yandex.ru>
2023-06-07 22:51:14 +0700

    feat: сделать состояние element__like active

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let popup = document.querySelector(".popup");
 let profileButtonInfo = document.querySelector(".profile__button-info");
 let popupCloseIcon = document.querySelector(".popup__button-close");
+let elementLike = document.querySelector(".element__like");
 
+//POP UP
+//описание функции открытие попапа
 function popupOpen() {
   popup.classList.add("popup_opened");
 }
-
+//описание функции закрытие попапа
 function popupClose() {
   popup.classList.remove("popup_opened");
 }
-//открытие попапа
+//вызов функции открытие попапа
 profileButtonInfo.addEventListener("click", popupOpen);
-//закрытие попапа
+//вызов функции закрытие попапа
 popupCloseIcon.addEventListener("click", popupClose);
