78d52bf1e33c92fc58fe1250354074387e2c9b90
Elena Vasileva <milena-iron@yandex.ru>
2023-06-08 17:35:04 +0700

    refactor: изменение порядка написания кода в js

--- script.js
+++ script.js
@@ -14,22 +14,19 @@ let popupCloseIcon = formElement.querySelector(".popup__button-close");
 let popupButtonSave = formElement.querySelector(".popup__button-save");
 
 //POP UP
-//ОТКРЫТИЕ
-//описание функции
+//ОТКРЫТИЕ и ЗАКРЫТИЕ
+//описание функций
 function popupOpen(modal) {
   modal.classList.add("popup_opened");
 }
-//вызов функции
-profileButtonInfo.addEventListener("click", function () {
-  popupOpen(popup);
-});
-
-//ЗАКРЫТИЕ
-//описание функции
 function popupClose(modal) {
   modal.classList.remove("popup_opened");
 }
-//вызов функций
+
+//вызовы функций
+profileButtonInfo.addEventListener("click", function () {
+  popupOpen(popup);
+});
 popupCloseIcon.addEventListener("click", function () {
   popupClose(popup);
 });
@@ -37,23 +34,29 @@ popupButtonSave.addEventListener("click", function () {
   popupClose(popup);
 });
 
+//ЗАПОЛНЕНИЕ ФОРМЫ
 //Значения на странице дублируем в форму
-function handleFormSubmit(evt) {
+function setInputText(evt) {
   nameInput.value = profileTitle.textContent.trim(); //.trim() - уберет лишние пробелы
   jobInput.value = profileSubTitle.textContent.trim();
 }
-profileButtonInfo.addEventListener("click", function () {
-  handleFormSubmit();
-});
 
 // Дублируем значения из формы в значения на странице
-function handleFormSubmit2(evt) {
+function setTextInput(evt) {
   profileTitle.textContent = nameInput.value;
   profileSubTitle.textContent = jobInput.value;
 }
-popupButtonSave.addEventListener("click", function () {
-  handleFormSubmit2();
-});
 
+//не дает отправить форму
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+}
+//Вызовы функций
+profileButtonInfo.addEventListener("click", function () {
+  setInputText();
+});
+popupButtonSave.addEventListener("click", function () {
+  setTextInput();
+});
 // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
 formElement.addEventListener("submit", handleFormSubmit);
