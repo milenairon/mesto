0ca6bc77458a97ac557741626fa25ac0a68eb35d
Elena Vasileva <milena-iron@yandex.ru>
2023-06-08 15:25:40 +0700

    feat: написать код в index.js

--- script.js
+++ script.js
@@ -1,18 +1,59 @@
-let popup = document.querySelector(".popup");
-let profileButtonInfo = document.querySelector(".profile__button-info");
-let popupCloseIcon = document.querySelector(".popup__button-close");
+//Находим элементы секции profile
+let profile = document.querySelector(".profile");
+let profileButtonInfo = profile.querySelector(".profile__button-info");
+let profileTitle = profile.querySelector(".profile__title");
+let profileSubTitle = profile.querySelector(".profile__subtitle");
+//Находим элементы секции element
 let elementLike = document.querySelector(".element__like");
+//Находим элементы секции popup
+let popup = document.querySelector(".popup");
+let formElement = popup.querySelector(".popup__container");
+let nameInput = formElement.querySelector(".popup__name");
+let jobInput = formElement.querySelector(".popup__name-description");
+let popupCloseIcon = formElement.querySelector(".popup__button-close");
+let popupButtonSave = formElement.querySelector(".popup__button-save");
 
 //POP UP
-//описание функции открытие попапа
-function popupOpen() {
-  popup.classList.add("popup_opened");
+//ОТКРЫТИЕ
+//описание функции
+function popupOpen(modal) {
+  modal.classList.add("popup_opened");
 }
-//описание функции закрытие попапа
-function popupClose() {
-  popup.classList.remove("popup_opened");
+//вызов функции
+profileButtonInfo.addEventListener("click", function () {
+  popupOpen(popup);
+});
+
+//ЗАКРЫТИЕ
+//описание функции
+function popupClose(modal) {
+  modal.classList.remove("popup_opened");
 }
-//вызов функции открытие попапа
-profileButtonInfo.addEventListener("click", popupOpen);
-//вызов функции закрытие попапа
-popupCloseIcon.addEventListener("click", popupClose);
+//вызов функций
+popupCloseIcon.addEventListener("click", function () {
+  popupClose(popup);
+});
+popupButtonSave.addEventListener("click", function () {
+  popupClose(popup);
+});
+
+//Значения на странице дублируем в форму
+function handleFormSubmit(evt) {
+  nameInput.value = profileTitle.textContent.trim(); //.trim() - уберет лишние пробелы
+  jobInput.value = profileSubTitle.textContent.trim();
+}
+profileButtonInfo.addEventListener("click", function () {
+  handleFormSubmit();
+});
+
+// Дублируем значения из формы в значения на странице
+function handleFormSubmit2(evt) {
+  profileTitle.textContent = nameInput.value;
+  profileSubTitle.textContent = jobInput.value;
+}
+popupButtonSave.addEventListener("click", function () {
+  handleFormSubmit2();
+});
+
+// Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
+formElement.addEventListener("submit", handleFormSubmit);
