cb41a8f6baeea7e476e73ac7568024e218f1e0f0
Elena Vasileva <milena-iron@yandex.ru>
2023-06-12 14:02:09 +0700

    refactor: переписать код в документе js более рационально, убрать неиспользуемые переменные

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ let profile = document.querySelector(".profile");
 let profileButtonInfo = profile.querySelector(".profile__button-info");
 let profileTitle = profile.querySelector(".profile__title");
 let profileSubTitle = profile.querySelector(".profile__subtitle");
-//Находим элементы секции element
-let elementLike = document.querySelector(".element__like");
 //Находим элементы секции popup
 let popup = document.querySelector(".popup");
 let formElement = popup.querySelector(".popup__container");
@@ -15,47 +13,38 @@ let popupButtonSave = formElement.querySelector(".popup__button-save");
 
 //POP UP
 //ОТКРЫТИЕ и ЗАКРЫТИЕ
-//описание функций
 function popupOpen(modal) {
   modal.classList.add("popup_opened");
 }
 function popupClose(modal) {
   modal.classList.remove("popup_opened");
 }
-
-//вызовы функций
-profileButtonInfo.addEventListener("click", function () {
-  popupOpen(popup);
-});
-popupCloseIcon.addEventListener("click", function () {
-  popupClose(popup);
-});
-popupButtonSave.addEventListener("click", function () {
-  popupClose(popup);
-});
-
 //ЗАПОЛНЕНИЕ ФОРМЫ
 //Значения на странице дублируем в форму
 function setInputText(evt) {
   nameInput.value = profileTitle.textContent.trim(); //.trim() - уберет лишние пробелы
   jobInput.value = profileSubTitle.textContent.trim();
 }
-
 // Дублируем значения из формы в значения на странице
 function setTextInput(evt) {
   profileTitle.textContent = nameInput.value;
   profileSubTitle.textContent = jobInput.value;
 }
-
 //не дает отправить форму
 function handleFormSubmit(evt) {
   evt.preventDefault();
 }
-//Вызовы функций
+
+//ВЫЗОВЫ ФУНКЦИЙ
+popupCloseIcon.addEventListener("click", function () {
+  popupClose(popup);
+});
 profileButtonInfo.addEventListener("click", function () {
+  popupOpen(popup);
   setInputText();
 });
 popupButtonSave.addEventListener("click", function () {
+  popupClose(popup);
   setTextInput();
 });
 // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
