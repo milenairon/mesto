3352c9b10d34ae6069936958f092195e27847671
Elena Vasileva <milena-iron@yandex.ru>
2023-06-22 13:49:13 +0700

    feat: создать попап добавления карточек (без работающих кнопок создать и закрыть)

--- index.js
+++ index.js
@@ -26,10 +26,11 @@ const initialCards = [
   }
 ];
 //Находим элементы секции profile
-let profile = document.querySelector(".profile");
-let profileButtonInfo = profile.querySelector(".profile__button-info");
-let profileTitle = profile.querySelector(".profile__title");
-let profileSubTitle = profile.querySelector(".profile__subtitle");
+const profile = document.querySelector(".profile");
+const profileButtonInfo = profile.querySelector(".profile__button-info");
+const profileButtonAdd = profile.querySelector(".profile__button-add");
+const profileTitle = profile.querySelector(".profile__title");
+const profileSubTitle = profile.querySelector(".profile__subtitle");
 //Находим элементы секции element
 const elementElement = document.querySelector('.element');
 const templateElement = document.querySelector('#element-template').content.querySelector('.element__item');
@@ -37,12 +38,14 @@ const templateElement = document.querySelector('#element-template').content.quer
 
 //Находим элементы секции popup
 let popup = document.querySelector(".popup");
+const popupForm = document.querySelector(".popup-form");
+const popupAdd = document.querySelector(".popup-add");
 let formElement = popup.querySelector(".popup__container");
 let nameInput = formElement.querySelector(".popup__name");
 let jobInput = formElement.querySelector(".popup__job");
-let popupCloseIcon = formElement.querySelector(".popup__button-close");
-let popupButtonSave = formElement.querySelector(".popup__button-save");
+const popupCloseIcon = document.querySelector(".popup__button-close");
 
+let popupButtonSave = formElement.querySelector(".popup__button-save");
 //POP UP
 //ОТКРЫТИЕ и ЗАКРЫТИЕ
 function popupOpen(modal) {
@@ -51,6 +54,8 @@ function popupOpen(modal) {
 function popupClose(modal) {
   modal.classList.remove("popup_opened");
 }
+
+
 //ЗАПОЛНЕНИЕ ФОРМЫ
 //Значения на странице дублируем в форму
 function setInputText(evt) {
@@ -67,7 +72,29 @@ function handleFormSubmit(evt) {
   evt.preventDefault();
 }
 
+//
 
+
+
+
+//ВЫЗОВЫ ФУНКЦИЙ
+popupCloseIcon.addEventListener("click", function () {
+  popupClose(popup);
+});
+profileButtonInfo.addEventListener("click", function () {
+  popupOpen(popupForm);
+  setInputText();
+});
+profileButtonAdd.addEventListener("click", function () {
+  popupOpen(popupAdd);
+});
+popupButtonSave.addEventListener("click", function () {
+  popupClose(popup);
+  setTextInput();
+});
+
+// Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
+formElement.addEventListener("submit", handleFormSubmit);
 
 
 
@@ -101,7 +128,7 @@ return cloneElementElement;
 
 //добавить в ul(elementElement) все, что в функции create
 function render (data){
-elementElement.append(create(data));
+elementElement.prepend(create(data));
 }
 
 //Вызываем функцию render для всех элементов массива initialCards перебором(forEach)
@@ -137,20 +164,7 @@ initialCards.forEach(item => {
 
 
 
-//ВЫЗОВЫ ФУНКЦИЙ
-popupCloseIcon.addEventListener("click", function () {
-  popupClose(popup);
-});
-profileButtonInfo.addEventListener("click", function () {
-  popupOpen(popup);
-  setInputText();
-});
-popupButtonSave.addEventListener("click", function () {
-  popupClose(popup);
-  setTextInput();
-});
-// Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
-formElement.addEventListener("submit", handleFormSubmit);
+
 
 
 
